ee0960c3e84e2a1ca245c6bc4ff0839d3ec815c3
NoFlex7 <sadullaernazarovich@gmail.com>
2025-12-23 14:52:43 +0500

    done

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors"; // <-- cors ni import qilamiz
 import adminRoutes from "./rules/admin.js";
 
 dotenv.config();
 const app = express();
 
+app.use(cors()); // <-- CORS middleware ni qo'shamiz
 app.use(express.json());
 
 mongoose
